6e2b7121f5960b2218e0b0b58f0e97920d33aff4
Mmy2000 <yahianada005@gmail.com>
2024-04-21 19:48:55 +0200

    starting with js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 let aboutOffset = $('#about').offset().top
 $(window).scroll(function () { 
-    
+    let scrolTop = $(window).scrollTop()
+    if (scrolTop > aboutOffset) {
+        $('nav').css('backgroundColor','#ffffff !important')
+    }else{
+        $('nav').css('backgroundColor','transparent !important')
+        console.log('hello');
+    }
 });
