d0dac87315fb92d670814f570717050adf36770d
Mmy2000 <yahianada005@gmail.com>
2024-04-25 16:54:24 +0200

    starting the project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let aboutOffset = $('#about').offset().top
 $(window).scroll(function () { 
     let scrolTop = $(window).scrollTop()
-    if (scrolTop > aboutOffset - 600) {
+    if (scrolTop > aboutOffset - 300) {
         $('nav').addClass('nav-style');
     }else{
         $('nav').removeClass('nav-style');
