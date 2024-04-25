55685688d6d8c725acd3f9a0b272654c64d5549b
Mmy2000 <yahianada005@gmail.com>
2024-04-25 16:50:54 +0200

    starting the project

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 let aboutOffset = $('#about').offset().top
 $(window).scroll(function () { 
     let scrolTop = $(window).scrollTop()
-    if (scrolTop > aboutOffset) {
+    if (scrolTop > aboutOffset - 600) {
         $('nav').addClass('nav-style');
     }else{
         $('nav').removeClass('nav-style');
         $('nav').addClass('bg-transparent');
     }
+    console.log(aboutOffset);
+    console.log(aboutOffset - 100);
 });
